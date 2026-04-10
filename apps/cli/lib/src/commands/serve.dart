3b0186c197109db763b8b2633b1b6e8254d08fc6
mrgnhnt96 <39742020+mrgnhnt96@users.noreply.github.com>
2026-04-10 08:32:47 -0700

    feat: set up initial cli

---
+++ apps/cli/lib/src/commands/serve.dart
@@ -0,0 +1,3 @@
+void serve() {
+  print('serving');
+}
--- apps/cli/lib/src/zonai_runner.dart
+++ apps/cli/lib/src/zonai_runner.dart
@@ -1,5 +1,32 @@
-import 'dart:io';
+import 'package:zonai_cli/src/commands/migrate.dart';
+import 'package:zonai_cli/src/commands/serve.dart';
+import 'package:zonai_cli/src/deps/args.dart';
+
+const _usage = '''
+Usage: zonai <command>
+
+Commands:
+  help        Show help information
+  version     Show version information
+  migrate     Migrate the database
+  serve       Serve the application
+''';
 
 Future<void> run() async {
-  exit(0);
+  if (args['help'] case null || true) {
+    print(_usage);
+    return;
+  }
+
+  if (args.path.isEmpty) {
+    print(_usage);
+    return;
+  }
+
+  switch (args.path) {
+    case ['migrate']:
+      migrate();
+    case ['serve']:
+      serve();
+  }
 }
