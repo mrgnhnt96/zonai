9c19729032addc0632ed9704ce0fd578983c5942
mrgnhnt96 <39742020+mrgnhnt96@users.noreply.github.com>
2026-05-11 13:09:30 -0700

    chore: gen migrations

--- apps/playground/.zonai/migrations/0003_update.sql
+++ apps/playground/.zonai/migrations/0003_update.sql
@@ -1,9 +1,28 @@
-ALTER TABLE "items" ALTER COLUMN "id" SET NOT NULL;
+CREATE TABLE "items_raindrop_rebuild" (
+  "id" TEXT PRIMARY KEY,
+  "body" TEXT NOT NULL,
+  "description" TEXT,
+  "status" INTEGER,
+  "created_at" INTEGER NOT NULL,
+  "updated_at" INTEGER
+);
+INSERT INTO "items_raindrop_rebuild" SELECT * FROM "items";
+DROP TABLE "items";
+ALTER TABLE "items_raindrop_rebuild" RENAME TO "items";
 
-ALTER TABLE "users" ALTER COLUMN "id" SET NOT NULL;
+CREATE TABLE "users_raindrop_rebuild" (
+  "email" TEXT NOT NULL,
+  "password" TEXT NOT NULL,
+  "id" TEXT PRIMARY KEY,
+  "name" TEXT NOT NULL,
+  "deleted_at" INTEGER
+);
+INSERT INTO "users_raindrop_rebuild" SELECT * FROM "users";
+DROP TABLE "users";
+ALTER TABLE "users_raindrop_rebuild" RENAME TO "users";
 
 ALTER TABLE "users" RENAME COLUMN "deleted_at" TO "updated_at";
 
-ALTER TABLE "users" ADD COLUMN "created_at" INTEGER NOT NULL;
+ALTER TABLE "users" ADD COLUMN "created_at" INTEGER NOT NULL DEFAULT 0;
 
 CREATE UNIQUE INDEX "users.id_unique" ON "users" ("id");
