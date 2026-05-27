973a40fa5f728701686cfc120247e147d5ff1c24
mrgnhnt96 <39742020+mrgnhnt96@users.noreply.github.com>
2026-05-27 14:42:51 -0700

    fix: rename to otp code

--- libs/zonai_schema/lib/src/handlers/messages/deps/__email.dart
+++ libs/zonai_schema/lib/src/handlers/messages/deps/__email.dart
@@ -55,7 +55,7 @@ class _SendEmail {
     _sendBuiltIn(.passwordReset, to, collection, variables);
   }
 
-  void optCode(
+  void otpCode(
     EmailAddress to, {
     required String collection,
     Map<String, dynamic>? variables,
