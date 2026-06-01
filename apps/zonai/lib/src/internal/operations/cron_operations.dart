47696e3e7fd3e57328711fdd8fa16735c98b53b9
mrgnhnt96 <39742020+mrgnhnt96@users.noreply.github.com>
2026-06-01 14:36:31 -0700

    feat: add cron operations

---
+++ apps/zonai/lib/src/internal/operations/cron_operations.dart
@@ -0,0 +1,8 @@
+import 'package:zonai/src/internal/tables/crons_table.dart';
+import 'package:zonai_schema/src/operations/table_operations.dart';
+
+final class CronOperations extends TableOperations<CronsTable, CronEntry> {
+  CronOperations() : super(crons);
+}
+
+CronOperations main() => CronOperations();
