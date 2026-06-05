26bba2c5d5eeaab8510b4975b2136221404d4f62
mrgnhnt96 <39742020+mrgnhnt96@users.noreply.github.com>
2026-06-04 23:36:28 -0700

    feat: add canCreate abuser for admin

--- apps/zonai/lib/src/internal/rules/abuser_table_rules.dart
+++ apps/zonai/lib/src/internal/rules/abuser_table_rules.dart
@@ -1,7 +1,6 @@
 import 'package:zonai/src/internal/tables/abusers_table.dart';
 import 'package:zonai/src/internal/rules/internal_rules.dart';
-import 'package:zonai_schema/src/internal/abusers_table.dart'
-    show AbuserEntry;
+import 'package:zonai_schema/src/internal/abusers_table.dart' show AbuserEntry;
 import 'package:zonai_schema/src/types/jwt.dart';
 
 AbuserTableRules main() => AbuserTableRules();
@@ -9,6 +8,12 @@ AbuserTableRules main() => AbuserTableRules();
 final class AbuserTableRules
     extends InternalTableRules<AbusersTable, AbuserEntry> {
   AbuserTableRules() : super(abusers);
+
+  @override
+  Future<bool> canCreate(Jwt? jwt) async => switch (jwt?.admin.canEdit) {
+    true => true,
+    _ => false,
+  };
 
   @override
   Future<bool> canDelete(Jwt? jwt) async => switch (jwt?.admin.canEdit) {
